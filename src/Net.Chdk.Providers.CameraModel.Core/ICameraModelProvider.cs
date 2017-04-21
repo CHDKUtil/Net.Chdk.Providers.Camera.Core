592ef64ceccc4560407ebc0d276c84a996d1591e
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 18:40:45 +0300

    Add solution

---
+++ src/Net.Chdk.Providers.CameraModel.Core/ICameraModelProvider.cs
@@ -0,0 +1,10 @@
+﻿using Net.Chdk.Model.Camera;
+using Net.Chdk.Model.CameraModel;
+
+namespace Net.Chdk.Providers.CameraModel
+{
+    public interface ICameraModelProvider
+    {
+        CameraModelInfo[] GetCameraModels(CameraInfo cameraInfo);
+    }
+}
