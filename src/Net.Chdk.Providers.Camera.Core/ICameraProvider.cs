7ac2642ea82072304ce6d0ed4458dae2e97d611d
Dmitry Shechtman <damnian@gmail.com>
2017-07-14 04:48:34 +0300

    Add GetCameraModels(SoftwareProductInfo productInfo, SoftwareCameraInfo cameraInfo)

--- src/Net.Chdk.Providers.Camera.Core/ICameraProvider.cs
+++ src/Net.Chdk.Providers.Camera.Core/ICameraProvider.cs
@@ -7,6 +7,7 @@ namespace Net.Chdk.Providers.Camera
     public interface ICameraProvider
     {
         CameraModelsInfo GetCameraModels(CameraInfo cameraInfo);
+        CameraModelsInfo GetCameraModels(SoftwareProductInfo productInfo, SoftwareCameraInfo cameraInfo);
         SoftwareCameraInfo GetCamera(string productName, CameraInfo cameraInfo, CameraModelInfo cameraModelInfo);
         SoftwareEncodingInfo GetEncoding(SoftwareProductInfo productInfo, SoftwareCameraInfo cameraInfo);
     }
